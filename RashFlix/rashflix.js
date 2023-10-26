fd85997e4028ad3ca37ce5047fba496bb52f80e9
sabinabakirzada <145649911+sabinabakirzada@users.noreply.github.com>
2023-10-26 13:28:03 +0400

    create rashflix

---
+++ RashFlix/rashflix.js
@@ -0,0 +1,4 @@
+const nameInp= document.getElementById("name")
+const nameInp= document.getElementById("Genre")
+const nameInp= document.getElementById("name")
+const nameInp= document.getElementById("name")
